eae0eb1bef26fea656975308d6864a7fc02ce228
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-29 15:22:50 -0700

    Update desired skills

--- constants/SkillsData.js
+++ constants/SkillsData.js
@@ -1,8 +1,7 @@
 import { FaPython, FaJava, FaLinux, FaAws, FaJira } from 'react-icons/fa';
 import { BsGit, BsGithub, BsTable } from "react-icons/bs";
 import { GrMysql } from "react-icons/gr";
-import { SiPostgresql, SiAmazonaws, SiLua, SiMicrosoftexcel, SiPandas, SiNumpy, SiPytorch, SiR, SiConfluence } from "react-icons/si";
-import { BiTable } from "react-icons/bi";
+import { SiPostgresql, SiAmazonredshift, SiAmazons3, SiTableau, SiLooker, SiLua, SiMicrosoftexcel, SiGooglebigquery, SiPandas, SiNumpy, SiPytorch, SiR, SiConfluence } from "react-icons/si";
 
 export const TechStackData = [
   {
@@ -22,26 +21,6 @@ export const TechStackData = [
     icon: <SiPytorch className="md:text-4xl text-2xl" color="#EE4C2C" />
   },
   {
-    name: "separator" },
-  {
-    name: "MySQL",
-    icon: <GrMysql className="md:text-4xl text-2xl" color="#08668e" />
-  },
-  {
-    name: "PostgreSQL",
-    icon: <SiPostgresql className="md:text-4xl text-2xl" color="#336791" />
-  },
-  {
-    name: "RedshiftSQL",
-    icon: <FaAws className="md:text-4xl text-2xl" color="#FF9900" />
-  },
-  {
-    name: "ETL",
-    icon: <BsTable className="md:text-4xl text-2xl" color="#000000" />
-  },
-  {
-    name: "separator"},
-  {
     name: "R",
     icon: <SiR className="md:text-4xl text-2xl" color="#276DC3" />
   },
@@ -54,15 +33,33 @@ export const TechStackData = [
     icon: <SiLua className="md:text-4xl text-2xl" color="#000080" />
   },
   {
-    name: "Linux",
-    icon: <FaLinux className="md:text-4xl text-2xl" color="#000000" />
+    name: "separator" },
+  {
+    name: "MySQL",
+    icon: <GrMysql className="md:text-4xl text-2xl" color="#08668e" />
+  },
+  {
+    name: "PostgreSQL",
+    icon: <SiPostgresql className="md:text-4xl text-2xl" color="#336791" />
+  },
+  {
+    name: "RedshiftSQL",
+    icon: <SiAmazonredshift className="md:text-4xl text-2xl" color="#276DC3" />
+  },
+  {
+    name: "BigQuery",
+    icon: <SiGooglebigquery className="md:text-4xl text-2xl" color="#4285F4" />
+  },
+  {
+    name: "ETL",
+    icon: <BsTable className="md:text-4xl text-2xl" color="#000000" />
   },
   {
     name: "separator"
   },
   {
     name: "AWS S3 CLI",
-    icon: <FaAws className="md:text-4xl text-2xl" color="#FF9900" />
+    icon: <SiAmazons3 className="md:text-4xl text-2xl" color="#FF9900" />
   },
   {
     name: "Git",
@@ -73,7 +70,15 @@ export const TechStackData = [
     icon: <BsGithub className="md:text-4xl text-2xl" color="black" />
   },
   {
+    name: "Linux",
+    icon: <FaLinux className="md:text-4xl text-2xl" color="#000000" />
+  },
+  {
     name: "separator"
+  },
+  {
+    name: "Tableau",
+    icon: <SiTableau className="md:text-4xl text-2xl" color="#E97627" />
   },
   {
     name: "Excel",
@@ -81,11 +86,7 @@ export const TechStackData = [
   },
   {
     name: "Looker",
-    icon: <BiTable className="md:text-4xl text-2xl" color="#000000" />
-  },
-  {
-    name: "Tableau",
-    icon: <BiTable className="md:text-4xl text-2xl" color="#E97627" />
+    icon: <SiLooker className="md:text-4xl text-2xl" color="#000000" />
   },
   {
     name: "Jira",
