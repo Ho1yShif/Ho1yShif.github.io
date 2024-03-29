701173d0514a05b6d6a3d56689eb2a3e62ebb230
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-29 14:45:12 -0700

    Flatten tech stack to a single section

--- sections/Skills.jsx
+++ sections/Skills.jsx
@@ -5,49 +5,16 @@ import { FaLaptopCode } from "react-icons/fa";
 import { TechStackData } from "@/constants/SkillsData";
 
 const TechStack = () => {
-  const [section, setSection] = useState("Advance");
   const [sectionData, setSectionData] = useState([]);
-  const [istechStack, setIsTechStack] = useState(false);
   const techStackRef = useRef();
   const techBoxesRef = useRef();
-  const buttonsRef = useRef();
 
   useEffect(() => {
-    const getScreenWidth = () =>
-      window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth;
-
-    const techStackObserver = new IntersectionObserver(
-      ([techStackEntry]) => {
-        setIsTechStack(techStackEntry.isIntersecting);
-      },
-      {
-        rootMargin: `${getScreenWidth() <= 700 ? "-100px" : "-250px"}`,
-      }
-    );
-
-    techStackObserver.observe(techStackRef.current);
-
-    if (istechStack) {
-      techBoxesRef.current.classList.add("pop-up-child");
-      buttonsRef.current.classList.add("pop-up");
-    } else {
-      techBoxesRef.current.classList.remove("pop-up-child");
-      buttonsRef.current.classList.remove("pop-up");
-    }
-  }, [istechStack]);
-
-  useEffect(() => {
-    const selectedSection = TechStackData.find((sections) =>
-      sections.hasOwnProperty(section)
-    );
-    setSectionData(selectedSection ? selectedSection[section] : []);
-
-    setTimeout(() => {
-      techBoxesRef.current.classList.add("pop-up-child");
-    }, 300);
-  }, [section]);
+    const allTechStack = TechStackData.reduce((acc, curr) => {
+      return [...acc, ...curr.Advance, ...curr.Good, ...curr.Familiar];
+    }, []);
+    setSectionData(allTechStack);
+  }, []);
 
   return (
     <Fragment>
@@ -61,49 +28,7 @@ const TechStack = () => {
         </h2>
 
         <div
-          className='pop-down transition-all w-fit duration-500 m-auto rounded-lg border border-black dark:border-white border-solid overflow-hidden'
-          ref={buttonsRef}
-        >
-          <button
-            className={`w-[120px] md:w-[150px] p-2 font-bold ${
-              section === "Advance" ? "bg-red-600" : null
-            } transition-all`}
-            onClick={(e) => {
-              setSection(e.target.innerText);
-              if (section !== e.target.innerText)
-                techBoxesRef.current.classList.remove("pop-up-child");
-            }}
-          >
-            Advance
-          </button>
-          <button
-            className={`w-[120px] md:w-[150px] p-2 font-bold ${
-              section === "Good" ? "bg-red-600" : null
-            } transition-all border-l border-r border-black dark:border-white border-solid`}
-            onClick={(e) => {
-              setSection(e.target.innerText);
-              if (section !== e.target.innerText)
-                techBoxesRef.current.classList.remove("pop-up-child");
-            }}
-          >
-            Good
-          </button>
-          <button
-            className={`w-[100px] md:w-[150px] p-2 font-bold ${
-              section === "Familiar" ? "bg-red-600" : null
-            } transition-all`}
-            onClick={(e) => {
-              setSection(e.target.innerText);
-              if (section !== e.target.innerText)
-                techBoxesRef.current.classList.remove("pop-up-child");
-            }}
-          >
-            Familiar
-          </button>
-        </div>
-
-        <div
-          className='pop-down-child flex min-h-[450px] py-[30px] px-[20px] md:px-[100px] flex-wrap justify-center items-center gap-5'
+          className='flex min-h-[450px] py-[30px] px-[20px] md:px-[100px] flex-wrap justify-center items-center gap-5'
           ref={techBoxesRef}
         >
           {sectionData.map((tech) => (
