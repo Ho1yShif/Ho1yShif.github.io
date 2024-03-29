1ab139f7b0546cc5ccaa48dffb457206cbf5f30e
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-29 16:39:51 -0700

    Populate Experience

--- constants/ExperienceData.jsx
+++ constants/ExperienceData.jsx
@@ -1,11 +1,89 @@
 export const ExperienceData = [
   {
-    organization: "Good Tech MIND",
-    role: "MERN STACK INTERN",
-    from_date: "April 2023",
-    to_date: "June 2023",
+    organization: "Annalect",
+    role: "Data Scientist",
+    from_date: "January 2023",
+    to_date: "Current",
     description:
-      "In this internship, I worked on many projects, including an eCommerce application and a real-time chat system. Through this internship, I gained a strong command of Redux and Redux Toolkit. After completing this internship, I have gained the confidence and skills needed to develop a full-stack application for a real-world eCommerce site.",
+      `● Achieving $750K savings by diagnosing CPM pricing issues and developing comprehensive QA solution
+      ● Facilitating million-dollar marketing pitches through efficient data ETL, securing high-value contracts
+      ● Leveraging logistic regression machine learning modeling to build custom Nissan marketing audiences
+      ● Automating first-party Nissan sales data load using Python, achieving a 92% reduction in lead time
+      ● Owning and leveraging B2B data with 250M rows in Redshift, delivering targeted marketing audiences
+      ● Leading Confluence documentation of new processes and maintenance of existing datasets and scripts
+      ● Designing Tableau dashboards to track MoM Nissan model market demographics and uncover insights
+      ● Managing client expectations, translating business needs into data solutions that drive team decisions
+      `,
+    side: "left",
+  },
+  {
+    organization: "JPMorgan Chase & Co.",
+    role: "AI & Data Science Summer Analyst",
+    from_date: "June 2022",
+    to_date: "August 2022",
+    description:
+      `● Leveraged XGBoost algorithm in Python to improve wallet estimation regression model by 94.1%
+      ● Won 2022 Housing Affordability Challenge among 140 interns by implementing PCA and projecting HPI
+      ● Conducted literature review to research machine learning methods and strategies for wallet estimation
+      `,
+    side: "right",
+  },
+  {
+    organization: "Prose",
+    role: "Growth Data Analyst Intern",
+    from_date: "January 2022",
+    to_date: "May 2022",
+    description:
+      `● Built last-click attribution model in BigQuery to track $40M annual marketing spend across channels
+      ● Automated Looker dashboard based on attribution model to report WoW and MoM channel metrics
+      ● Tested gift products to determine their impact on customer churn and drive stakeholder decisions
+      `,
+    side: "left",
+  },
+  {
+    organization: "Torpedo Software LLC",
+    role: "Technical Curriculum Writer",
+    from_date: "January 2023",
+    to_date: "Current",
+    description:
+      `● Designing curriculum and writing lessons for Lua Learning, an award-winning Roblox game with 4M+ visits and 115K monthly active users
+       ● Lua Learning: https://www.roblox.com/games/1334669864/Lua-Learning
+      `,
+    side: "right",
+  },
+  {
+    organization: "Crash Course (Complexly)",
+    role: "Technical Script Writer",
+    from_date: "November 2022",
+    to_date: "November 2023",
+    description:
+      `● Collaborated with YouTube and Arizona Statue University to write 9 episodes of Crash Course Study Hall: Intro to Java Code and Programming For Beginners, hosted by Sabrina Cruz
+       ● Crash Course Playlist Link: https://www.youtube.com/watch?v=yBFu9HxiD88&list=PLID58IQe16nFgbHGRCj5QEXKUpVIilpDN&index=29
+      `,
+    side: "left",
+  },
+  {
+    organization: "DataLemur.com",
+    role: "Technical Writer & Product Manager",
+    from_date: "August 2022",
+    to_date: "January 2023",
+    description:
+      `● Supported author Nick Singh’s data science platform and achieved 100K+ users within 10 months
+      ● Documented, tested, and improved 200+ SQL and mathematics problems, solutions, and hints
+      ● Instituted metadata tagging system and SQL style conventions to standardize the platform● Supported author Nick Singh’s data science platform and achieved 100K+ users within 10 months ● Documented, tested, and improved 200+ SQL and mathematics problems, solutions, and hints ● Instituted metadata tagging system and SQL style conventions to standardize the platform
+      `,
+    side: "right",
+  },
+  {
+    organization: "Cross River Bank",
+    role: "Data Technology Analyst Intern",
+    from_date: "June 2021",
+    to_date: "August 2021",
+    description:
+      `● Created Capacity Planning Model to streamline project costing for the entire IT department
+      ● Developed multinomial logistic regression model for loan classification in Python
+      ● Presented accomplishments throughout summer internship to CEO and C-Level managers
+      `,
     side: "left",
   },
 ]
