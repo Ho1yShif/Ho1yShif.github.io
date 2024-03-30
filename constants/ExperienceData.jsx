18c44e5f9469caf1426213257145ff39d2bdd77f
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-30 11:59:32 -0700

    Add career tracks to experiences

--- constants/ExperienceData.jsx
+++ constants/ExperienceData.jsx
@@ -13,7 +13,8 @@ export const ExperienceData = [
       ● Leading Confluence documentation of new processes and maintenance of existing datasets and scripts
       ● Designing Tableau dashboards to track MoM Nissan model market demographics and uncover insights
       ● Managing client expectations, translating business needs into data solutions that drive team decisions
-      `
+      `,
+      "career_track": ["Data Science", "Machine Learning", "Data Analytics", "Data Engineering", "Marketing", "Consulting", "Technical Writing"]
   },
   {
     organization: "Crash Course (Complexly)",
@@ -24,7 +25,8 @@ export const ExperienceData = [
       `● Collaborated with YouTube and Arizona Statue University to write 9 episodes of Crash Course Study Hall: Intro to Java Code and Programming For Beginners, hosted by Sabrina Cruz
        ● Crash Course Playlist Link:
        https://www.youtube.com/watch?v=yBFu9HxiD88&list=PLID58IQe16nFgbHGRCj5QEXKUpVIilpDN&index=29
-      `
+      `,
+      "career_track": ["Technical Writing", "Software Engineering", "Education"]
   },
   {
     organization: "Torpedo Software LLC",
@@ -34,7 +36,8 @@ export const ExperienceData = [
     description:
       `● Designing curriculum and writing lessons for Lua Learning, an award-winning Roblox game with 4M+ visits and 115K monthly active users
        ● Lua Learning Link: https://www.roblox.com/games/1334669864/Lua-Learning
-      `
+      `,
+      "career_track": ["Technical Writing", "Software Engineering", "Game Development", "Education"]
   },
   {
     organization: "DataLemur.com",
@@ -45,7 +48,8 @@ export const ExperienceData = [
       `● Supported author Nick Singh’s data science platform and achieved 100K+ users within 10 months
       ● Documented, tested, and improved 200+ SQL and mathematics problems, solutions, and hints
       ● Instituted metadata tagging system and SQL style conventions to standardize the platform
-      `
+      `,
+      "career_track": ["Technical Writing", "Data Science", "Data Analytics", "Product Management", "Education"]
   },
   {
     organization: "JPMorgan Chase & Co.",
@@ -56,7 +60,8 @@ export const ExperienceData = [
       `● Leveraged XGBoost algorithm in Python to improve wallet estimation regression model by 94.1%
       ● Won 2022 Housing Affordability Challenge among 140 interns by implementing PCA and projecting HPI
       ● Conducted literature review to research machine learning methods and strategies for wallet estimation
-      `
+      `,
+      "career_track": ["Data Science", "Machine Learning", "Finance"]
   },
   {
     organization: "Prose",
@@ -67,7 +72,8 @@ export const ExperienceData = [
       `● Built last-click attribution model in BigQuery to track $40M annual marketing spend across channels
       ● Automated Looker dashboard based on attribution model to report WoW and MoM channel metrics
       ● Tested gift products to determine their impact on customer churn and drive stakeholder decisions
-      `
+      `,
+      "career_track": ["Data Analytics", "Marketing"]
   },
   {
     organization: "Cross River Bank",
@@ -78,6 +84,7 @@ export const ExperienceData = [
       `● Created Capacity Planning Model to streamline project costing for the entire IT department
       ● Developed multinomial logistic regression model for loan classification in Python
       ● Presented accomplishments throughout summer internship to CEO and C-Level managers
-      `
+      `,
+      "career_track": ["Data Science", "Machine Learning", "Finance", "Business Intelligence"]
   }
 ]
