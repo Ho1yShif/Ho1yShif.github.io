92d0706d7cd6a0d620e0277edabc8536525f324a
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-30 14:20:14 -0700

    Add copyright footer

--- layout/footer/Footer.jsx
+++ layout/footer/Footer.jsx
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 const Footer = () => {
   return (
     <Fragment>
-      <div className="p-2 dark:bg-black bg-[#ecf8f9] font-sans text-lg text-center">
-        @Shiv 2023
+      <div className="p-2 text-white dark:bg-black bg-[#ecf8f9] font-sans text-lg text-left">
+      © Shifra Isaacs
       </div>
     </Fragment>
   );
