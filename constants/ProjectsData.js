4fa49ff948cf2b85a096d0ab0d97c46fa9ddccab
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-29 17:37:56 -0700

    Add projects

--- constants/ProjectsData.js
+++ constants/ProjectsData.js
@@ -1,29 +1,54 @@
 export const ProjectsData = [
     {
-        projectName: "Shovee : Personal Portfolio Generator",
-        liveUrl: "https://shovee.com",
-        githubUrl: "https://github.com/iamshiv007/shovee",
+        projectName: "Crash Course Study Hall: Code and Programming for Beginners",
+        liveUrl: "https://www.youtube.com/watch?v=n3egiMIpv7c&list=PLID58IQe16nFgbHGRCj5QEXKUpVIilpDN&index=26",
+        githubUrl: "https://github.com/Ho1yShif/Crash_Course_Study_Hall_Code_and_Programming_for_Beginners",
         projectImage: {
-            imageUrl: "http://res.cloudinary.com/dqfrtazgi/image/upload/v1696218285/yn3naeznjfgszsbbjr92.png"
+            imageUrl: "/images/shif_images/OOP_Thumbnail.jpeg"
         },
-        techs: ["NextJS", "TailwindCSS", "Redux Toolkit", "Cloudinary", "Intersection Objerver"]
+        techs: ["Java", "Technical Writing", "OOP", "Data Structures", "Defensive Programming"]
     },
     {
-        projectName: "YouTube clone",
-        liveUrl: "https://youtube-clone-shivraj.vercel.app",
-        githubUrl: "https://github.com/iamshiv007/yotube-clone",
+        projectName: "DataLemur",
+        liveUrl: "DataLemur.com",
+        githubUrl: "",
         projectImage: {
-            imageUrl: "http://res.cloudinary.com/dqfrtazgi/image/upload/v1696218286/t1zjvlwogccwit9888w7.png"
+            imageUrl: "/images/shif_images/DataLemur.jpeg"
         },
-        techs: ["ReactJS", "ContextAPI", "ChakraUI", "YoutubeAPI", "ContextAPI"]
+        techs: ["PostgreSQL", "Technical Writing", "Data Analysis", "Data Science", "Mathematics", "Probability"]
     },
     {
-        projectName: "Eccommerce Application",
-        liveUrl: "https://ecommerce-application-shivraj.vercel.app",
-        githubUrl: "https://github.com/iamshiv007/ecommerce-frontend",
+        projectName: "Classification of Fall Out Boy Eras",
+        liveUrl: "https://docs.google.com/document/d/1r6A2JHU6jtBoxAq3eWm2eP08l_u6KRtKTEpcoQXS84I/edit?usp=sharing",
+        githubUrl: "https://github.com/Ho1yShif/FOB_LR_Public",
         projectImage: {
-            imageUrl: "http://res.cloudinary.com/dqfrtazgi/image/upload/v1696220173/lnnt6nmesjvvmo3nx9ep.png"
+            imageUrl: "/images/shif_images/FOB.jpg"
         },
-        techs: ["ReactJS", "NodeJS", "Express", "MongoDB", "Redux Toolkit"]
-    }
+        techs: ["Python", "Pandas", "Numpy", "PyTorch", "Scikit-learn", "APIs", "Logistic Regression"]
+    },
+    {
+        projectName: "Lua Learning",
+        liveUrl: "https://www.roblox.com/games/1334669864/Lua-Learning",
+        projectImage: {
+            imageUrl: "/images/shif_images/Lua_Learning.png"
+        },
+        techs: ["Lua", "Technical Writing"]
+    },
+    {
+        projectName: "Shakespeare GPT",
+        githubUrl: "https://github.com/Ho1yShif/nanogpt",
+        projectImage: {
+            imageUrl: "/images/shif_images/Shakespeare_GPT.webp"
+        },
+        techs: ["Python", "PyTorch", "Machine Learning", "LLMs"]
+    },
+    {
+        projectName: "NJ Flood Prediction and Mitigation",
+        liveUrl: "https://docs.google.com/presentation/d/1_0ddFciVmtjiIOLNdVsPbGIlbC8DeLVuO9G7LaevThA/edit?usp=sharing",
+        githubUrl: "NJ Flood Prediction and Mitigation",
+        projectImage: {
+            imageUrl: "/images/shif_images/flood_nj.jpeg"
+        },
+        techs: ["Python", "Time Series", "ArcGIS", "XGBoost", "LSTM"]
+    },
 ]
