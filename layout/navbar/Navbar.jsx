f4c744e847701f1ec4984ac502950a2a16643afc
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-29 19:16:22 -0700

    Link name on left to homepage

--- layout/navbar/Navbar.jsx
+++ layout/navbar/Navbar.jsx
@@ -24,9 +24,9 @@ const Navbar = () => {
         style={{ top: "0" }}
       >
         {/* Name Logo */}
-        <p className='text-gray-400 flex'>
-          <span className='text-lg font-bold'>SHIV</span> <DiTechcrunch />
-        </p>
+          <Link href="#home" className='text-gray-400 flex'>
+            <span className='text-lg font-bold'>Shifra Isaacs</span>
+          </Link>
         <div className='h-full flex gap-4'>
           {/* Navbar Links */}
           {NavbarMenu.map((navbar) => (
