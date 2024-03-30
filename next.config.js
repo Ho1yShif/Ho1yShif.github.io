b68da189faea683eb17626fd858dcb758fcf4252
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-30 12:09:14 -0700

    Current favicon situation

--- next.config.js
+++ next.config.js
@@ -6,7 +6,15 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  webpack5: true,
+  swcMinify: true
 }
 
 module.exports = nextConfig
