7958accc2191cfd75cb086f77940aa8a1b90902c
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-29 17:53:17 -0700

    Add Resume icon

--- utils/SocialMedia.jsx
+++ utils/SocialMedia.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from "react";
 import Link from "next/link";
-import { MdFeedback,  MdEmail } from "react-icons/md";
+import { MdEmail, MdPageview } from "react-icons/md";
+import { FaFileAlt } from "react-icons/fa";
 
 import { SocialMediaData } from "@/constants/SocialMediaData";
 import { PortfolioContext } from "@/contextApi/PortfolioContext";
@@ -10,20 +11,29 @@ const SocialMedia = () => {
 
   return (
     <Fragment>
-      <div className='absolute left-14 bottom-10 flex flex-col gap-4 z-10'>
+      <div className='absolute left-14 bottom-10 flex gap-4 z-10'>
         <a href="mailto:shifraisaacs@gmail.com" style={{ color: 'inherit', textDecoration: 'none' }}>
           <button
-            className='p-2 rounded-full text-xl bg-black text-white'
+            className='p-2 rounded-full text-4xl bg-black text-white'
             onClick={() => setShowModal(true)}
             title="Email"
           >
             <MdEmail />
           </button>
         </a>
+        <a href="https://drive.google.com/file/d/13f_wkFv5v536YB5onTxRraPWiKfjUhSG/view" style={{ color: 'inherit', textDecoration: 'none' }}>
+          <button
+            className='p-2 rounded-full text-4xl bg-black text-white'
+            onClick={() => setShowModal(true)}
+            title="Resume"
+          >
+            <FaFileAlt />
+          </button>
+        </a>
         {SocialMediaData.map((social, key) => (
           <Link className='w-fit' href={social.link} key={key}>
             <div
-              className={"p-2 rounded-full text-xl text-white"}
+              className={"p-2 rounded-full text-4xl text-white"}
               style={{ background: social.color }}
               title={social.name}
             >
