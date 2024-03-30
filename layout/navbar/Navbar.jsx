9ff4f57fa56dca104eb8c664a9bdde7dc3dd2d43
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-29 19:51:41 -0700

    Make navbar stay on top instead of disappearing

--- layout/navbar/Navbar.jsx
+++ layout/navbar/Navbar.jsx
@@ -1,9 +1,6 @@
 "use client";
-import React, { Fragment, useContext, useEffect, useState } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import Link from "next/link";
-import { DiTechcrunch } from "react-icons/di";
-import { BsFillLightningChargeFill } from "react-icons/bs";
-import { TbBulbFilled } from "react-icons/tb";
 
 import MobileNavbar from "./SidebarMobile";
 import { NavbarMenu } from "./NavbarItems";
@@ -12,45 +9,20 @@ import NavbarMobile from "./NavbarMobile";
 import { ThemeContext } from "@/context/themeContext";
 
 const Navbar = () => {
-  const [top, setTop] = useState("0");
   const [showMenu, setShowMenu] = useState(false);
 
   const { setThemeFun, theme } = useContext(ThemeContext);
-
-  // Logic for Navbar Hide and Show on scrolling behaviour
-  useEffect(() => {
-    let prevScrollPos = window.scrollY;
-
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY;
-
-      if (prevScrollPos > currentScrollPos) {
-        setTop("0"); // Show the navbar
-      } else {
-        setTop("-80px"); // Hide the navbar
-      }
-
-      prevScrollPos = currentScrollPos;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      // Cleanup: Remove the event listener when the component unmounts
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <Fragment>
       {/* Desktop Header */}
       <div
         className='w-full h-[70px] px-8 bg-black dark:bg-black backdrop-filter backdrop-blur-lg hidden md:flex justify-between items-center gap-4 dark:shadow-gray-800 fixed z-10 transition-all duration-500'
-        style={{ top: top }}
       >
         {/* Name Logo */}
-      <Link href="/#home" className='text-gray-400 flex'>
-        <span className='text-lg font-bold'>Shifra Isaacs</span>
-      </Link>
+        <Link href="/#home" className='text-gray-400 flex'>
+          <span className='text-lg font-bold'>Shifra Isaacs</span>
+        </Link>
         <div className='h-full flex gap-4'>
           {/* Navbar Links */}
           {NavbarMenu.map((navbar) => (
@@ -73,7 +45,6 @@ const Navbar = () => {
         setThemeFun={setThemeFun}
         showMenu={showMenu}
         theme={theme}
-        top={top}
       />
 
       {/* SideMenu For Mobile Screen */}
