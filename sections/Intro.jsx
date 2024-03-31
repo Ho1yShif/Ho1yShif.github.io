43e8a6abeabb38471aacd1acb88af3fa1808d688
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-31 16:57:55 -0700

    Make intro stack vertically on mobile

--- sections/Intro.jsx
+++ sections/Intro.jsx
@@ -47,55 +47,57 @@ const Intro = () => {
       </Head>
       <section id='home'>
         <div
-          className='min-h-[100vh] overflow-x-hidden px-[20px] md:px-[200px] lg:px-[200px] pt-[80px] md:pt-0 md:flex items-center justify-between shadow-zinc-300 dark:shadow-zinc-700 shadow-sm'
+          className='min-h-[100vh] overflow-x-hidden px-[20px] md:px-[200px] lg:px-[200px] pt-[80px] md:pt-0 md:flex items-center justify-between shadow-zinc-300 dark:shadow-zinc-700 shadow-sm flex-col md:flex-row'
           ref={homeRef}
         >
           <div
-            className='translate-x-[-500px] transition-all duration-700 opacity-0'
+            className='translate-x-[-500px] transition-all duration-700 opacity-0 order-2 md:order-1'
             ref={introRef}
           >
-            <p className='py-2 text-2xl md:text-4xl font-semibold font-sans'>
-              Hi there, I'm Shif 🤝
-            </p>
-            {/* Profile Name */}
-            <p className='text-2xl md:text-4xl py-2 font-semibold font-sans'>
-              I&apos;m a
-              <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
-                {" "}
-                Data<span className='text-white'>|</span>
-              </span>
-              Scientist,
-              <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
-                {" "}
-                Data<span className='text-white'>|</span>
-              </span>
-              Analyst,
-              <br></br>
-              Technical
-              <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
-                {" "}
-                Writer,<span className='text-white'>|</span>
-              </span>
-              and
-              <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
-                {" "}
-                Educator<span className='text-white'>|</span>
-              </span>
-            </p>
+            <div>
+              <p className='py-2 text-2xl md:text-4xl font-semibold font-sans'>
+                Hi there, I'm Shif 🤝
+              </p>
+              {/* Profile Name */}
+              <p className='text-2xl md:text-4xl py-2 font-semibold font-sans'>
+                I&apos;m a
+                <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
+                  {" "}
+                  Data<span className='text-white'>|</span>
+                </span>
+                Scientist,
+                <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
+                  {" "}
+                  Data<span className='text-white'>|</span>
+                </span>
+                Analyst,
+                <br></br>
+                Technical
+                <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
+                  {" "}
+                  Writer,<span className='text-white'>|</span>
+                </span>
+                and
+                <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
+                  {" "}
+                  Educator<span className='text-white'>|</span>
+                </span>
+              </p>
+            </div>
+
             <div style={{ marginTop: '10em' }}>
               <SocialMedia />
             </div>
           </div>
 
-
           {/* Image */}
           <div
-          className={
-            "translate-x-[100px] transition-all opacity-0 duration-700 w-[180px] h-[500px] md:w-[400px] md:h-[400px] bg-contain m-auto md:m-0 mt-[40px] md:mt-0 bg-no-repeat"
-          }
-          ref={profileRef}
-          style={{ backgroundImage: "url(/images/shif_images/Circle_Headshot.svg)" }}
-        />
+            className={
+              "translate-x-[100px] transition-all opacity-0 duration-700 w-[180px] h-[500px] md:w-[400px] md:h-[400px] bg-contain m-auto md:m-0 mt-[40px] md:mt-0 bg-no-repeat order-1 md:order-2"
+            }
+            ref={profileRef}
+            style={{ backgroundImage: "url(/images/shif_images/Circle_Headshot.svg)" }}
+          />
         </div>
       </section>
     </Fragment>
