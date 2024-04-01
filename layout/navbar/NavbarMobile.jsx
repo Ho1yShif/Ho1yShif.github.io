ea4cf836ce314347b934a36cfd6f6691aead677a
Shifra Isaacs <shifraisaacs@gmail.com>
2024-04-01 08:11:17 -0700

    Add maxwidth to navbar

--- layout/navbar/NavbarMobile.jsx
+++ layout/navbar/NavbarMobile.jsx
@@ -7,25 +7,27 @@ const NavbarMobile = () => {
 
   return (
     <div
-      className='w-full h-[80px] px-8 bg-black dark:bg-black backdrop-filter backdrop-blur-lg md:hidden flex justify-between items-center gap-4 dark:shadow-gray-800 fixed z-10 transition-all duration-500'
+      className='w-full h-[80px] bg-black dark:bg-black backdrop-filter backdrop-blur-lg md:hidden fixed z-10 transition-all duration-500'
     >
-      {/* Name Logo */}
-      <Link href="/#home" className='text-gray-400 flex'>
-        <span className='text-1vm font-bold'>Shifra Isaacs</span>
-      </Link>
-      <div className='h-full flex gap-4'>
-        {/* Navbar Links */}
-        {NavbarMenu.map((navbar) => (
-          <Link
-            className={"text-white dark:text-white font-semibold text-1vm"}
-            href={navbar.link}
-            key={navbar.name}
-          >
-            <div className='h-full pb-1 hover:pb-0 px-1 flex items-center hover:border-b-4  border-white dark:border-white transition-all'>
-              {navbar.name}
-            </div>
-          </Link>
-        ))}
+      <div className='px-8 max-w-[800px] m-auto flex justify-between items-center gap-4 dark:shadow-gray-800'>
+        {/* Name Logo */}
+        <Link href="/#home" className='text-gray-400 flex'>
+          <span className='text-1vm font-bold'>Shifra Isaacs</span>
+        </Link>
+        <div className='h-full flex gap-4'>
+          {/* Navbar Links */}
+          {NavbarMenu.map((navbar) => (
+            <Link
+              className={"text-white dark:text-white font-semibold text-1vm"}
+              href={navbar.link}
+              key={navbar.name}
+            >
+              <div className='h-full pb-1 hover:pb-0 px-1 flex items-center hover:border-b-4  border-white dark:border-white transition-all'>
+                {navbar.name}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
