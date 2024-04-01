a4709fed14138dce1d5028978294f6a6afbfb29c
Shifra Isaacs <shifraisaacs@gmail.com>
2024-04-01 08:25:56 -0700

    Align intro items

--- sections/Intro.jsx
+++ sections/Intro.jsx
@@ -47,7 +47,7 @@ const Intro = () => {
         <title>Shifra's Portfolio</title>
       </Head>
       <section id='home' ref={homeRef} className="pt-20">
-        <div className='flex justify-center items-center gap-10 ml-20'>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-10 ml-20'>
           <div
             className='translate-x-[-500px] transition-all duration-700 opacity-0 order-1 md:order-1'
             ref={introRef}
@@ -58,14 +58,14 @@ const Intro = () => {
           {/* Image */}
           <div
             className={
-              "translate-x-[100px]transition-all opacity-0 duration-700 w-[180px] h-[500px] md:w-[400px] md:h-[400px] bg-contain m-auto md:m-0 mt-[0px] md:mt-0 bg-no-repeat order-2 md:order-2"
+              "translate-x-[100px] transition-all opacity-0 duration-700 w-[180px] h-[500px] md:w-[400px] md:h-[400px] bg-contain m-auto md:m-0 mt-[0px] md:mt-0 bg-no-repeat order-2 md:order-2"
             }
             ref={profileRef}
             style={{ backgroundImage: "url(/images/shif_images/Circle_Headshot.svg)" }}
           />
         </div>
 
-        <div className='order-3 md:order-last mt-12'>
+        <div className='order-3 md:order-last mt-5 md:mt-0'>
           <SocialMedia className="max-w-xs flex flex-wrap justify-between md:max-w-full sm:text-sm" />
         </div>
       </section>
