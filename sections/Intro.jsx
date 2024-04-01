d7dc386972141e2565a9e7e1ceda2ca31c143efb
Shifra Isaacs <shifraisaacs@gmail.com>
2024-03-31 17:20:58 -0700

    Refactor intro

--- sections/Intro.jsx
+++ sections/Intro.jsx
@@ -1,7 +1,12 @@
-"use client";
 import React, { Fragment, useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import SocialMedia from '../utils/SocialMedia';
+
+// Function to get screen width
+const getScreenWidth = () =>
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
 
 const Intro = () => {
   const [isHome, setIsHome] = useState(false);
@@ -12,11 +17,6 @@ const Intro = () => {
 
   // Intersection observer animation on scroll
   useEffect(() => {
-    const getScreenWidth = () =>
-      window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth;
-
     // Scroll Animation
     if (homeRef.current) {
       const homeObserver = new IntersectionObserver(
@@ -38,17 +38,16 @@ const Intro = () => {
         introRef.current.classList.remove("slide-in");
       }
     }
-  }, [homeRef, isHome]);
+  }, [isHome]);
 
   return (
     <Fragment>
       <Head>
         <title>Shifra&apos;s Portfolio</title>
       </Head>
-      <section id='home'>
+      <section id='home' ref={homeRef}>
         <div
           className='min-h-[100vh] overflow-x-hidden px-[20px] md:px-[200px] lg:px-[200px] pt-[80px] md:pt-0 md:flex items-center justify-between shadow-zinc-300 dark:shadow-zinc-700 shadow-sm flex-col md:flex-row'
-          ref={homeRef}
         >
           <div
             className='translate-x-[-500px] transition-all duration-700 opacity-0 order-1 md:order-1'
@@ -88,12 +87,12 @@ const Intro = () => {
             <div className='order-3' style={{ marginTop: '10em' }}>
               <SocialMedia />
             </div>
-          </div> {/* This closing tag was missing */}
+          </div>
 
           {/* Image */}
           <div
             className={
-              "translate-x-[100px] transition-all opacity-0 duration-700 w-[180px] h-[500px] md:w-[400px] md:h-[400px] bg-contain m-auto md:m-0 mt-[40px] md:mt-0 bg-no-repeat order-2 md:order-2"
+              "translate-x-[100px] transition-all opacity-0 duration-700 w-[180px] h-[500px] md:w-[400px] md:h-[400px] bg-contain m-auto md:m-0 mt-[40px] md:mt-0 bg-no-repeat order-3 md:order-2"
             }
             ref={profileRef}
             style={{ backgroundImage: "url(/images/shif_images/Circle_Headshot.svg)" }}
