8fecae2203c4dd07a6bfec32c17e7c65a52e3283
Shifra Isaacs <shifraisaacs@gmail.com>
2024-04-02 08:15:16 -0700

    Remove data analyst

--- utils/IntroParagraph.jsx
+++ utils/IntroParagraph.jsx
@@ -2,29 +2,26 @@ import React from 'react';
 
 const IntroParagraph = () => (
   <div>
-    <p className='py-2 text-2xl md:text-4xl font-semibold font-sans'>
+    <p className='py-2 text-2xl md:text-4xl font-semibold font-sans text-centers'>
       Hi there, I'm Shif 🤝
     </p>
     {/* Profile Name */}
-    <p className='text-2xl md:text-4xl py-2 font-semibold font-sans  text-left'>
+    <p className='text-2xl md:text-4xl py-2 font-semibold font-sans text-center'>
       I&apos;m a
       <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
         {" "}
         Data<span className='text-white'>|</span>
       </span>
       Scientist,
-      <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
-        {" "}
-        Data<span className='text-white'>|</span>
-      </span>
-      Analyst,
     </p>
-    <p className='text-2xl md:text-4xl py-2 font-semibold font-sans text-left'>
+    <p className='text-2xl md:text-4xl py-2 font-semibold font-sans text-center'>
       Technical
       <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
         {" "}
         Writer,<span className='text-white'>|</span>
       </span>
+    </p>
+    <p className='text-2xl md:text-4xl py-2 font-semibold font-sans text-center'>
       and
       <span className='text-[#0c11ab] dark:text-[#0c11ab]'>
         {" "}
